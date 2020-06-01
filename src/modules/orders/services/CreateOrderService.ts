3261e80d2fd5559cf54e407ffe8664676c980e6b
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-05-31 21:56:03 -0400

    first commit

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,62 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not find');
+    }
+
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (findProducts.length === 0) {
+      throw new AppError('Could not find products');
+    }
+
+    const productsNotAvailable = products.filter(
+      product =>
+        findProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (productsNotAvailable.length) {
+      throw new AppError('Quantity of products is not available');
+    }
+
+    const productsQuantityUpdate = products.map(product => ({
+      id: product.id,
+      quantity:
+        findProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(productsQuantityUpdate);
+
+    const productsFormatted = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: findProducts.filter(
+        productFiltered => productFiltered.id === product.id,
+      )[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsFormatted,
+    });
+
+    return order;
   }
 }
 
